b82e707db30ec8a0548e7ea61fd45f55d94a909c
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-02-02 11:51:46 -0800

    fix auth middleware

--- middleware/auth-user.js
+++ middleware/auth-user.js
@@ -10,23 +10,24 @@ exports.authenticateUser = async (req, res, next) => {
   const credentials = auth(req);
 
   // if the user's credentials are available... check if user exists by email
-  if (credentials) { 
-    const user = await User.findOne({ where: { emailAddress: credentials.emailAddress } });
-      if (user) { // if user is found in db
-        const authenticated = bcryptjs.compareSync(credentials.pass, user.password);
-          if (authenticated) { // if passwords match
-            console.log(`Authentication successful for username: ${user.emailAddress}`);
+  // NOTE: that credentials username and password get returned as .name and .pass
+  if (credentials) {
+    const user = await User.findOne({ where: { emailAddress: credentials.name } });
+    if (user) { // if user is found in db
+      const authenticated = bcryptjs.compareSync(credentials.pass, user.password);
+      if (authenticated) { // if passwords match
+        console.log(`Authentication successful for username: ${user.emailAddress}`);
 
-            // store the user on the Request object
-            req.currentUser = user;
-          } else {
-            // not authenticated 
-            message = `Authentication failure for ${user.emailAddress}`;
-          }
+        // store the user on the Request object
+        req.currentUser = user;
       } else {
-        // user not found
-        message = `User not found for email: ${user.emailAddress}`;
+        // not authenticated 
+        message = `Authentication failure for ${user.emailAddress}`;
       }
+    } else {
+      // user not found
+      message = `User not found for email: ${user.emailAddress}`;
+    }
   } else {
     // auth header not found
     message = 'Authorization header not found';
