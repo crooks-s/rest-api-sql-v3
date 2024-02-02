b92fc9dc0c185fdd05f9a8d52e07a4f8e38ac1a6
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-02-01 19:47:56 -0800

    add courses route validation for post new course

--- routes/courses-route.js
+++ routes/courses-route.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const { asyncHandler } = require('../middleware/async-handler');
+const { check, validationResult } = require('express-validator');
 
 // construct router instance
 const router = express.Router();
@@ -9,7 +10,7 @@ const router = express.Router();
 // GET route that will return all courses
 // including the User associated with each course
 // and 200 code
-router.get('/courses', asyncHandler(async(req, res) => {
+router.get('/courses', asyncHandler(async (req, res) => {
   res.status(200).json({ message: '/courses got GET! nice' });
 }));
 
@@ -23,19 +24,23 @@ router.get('/courses/:id', asyncHandler(async (req, res) => {
 // POST route that will create a new course,
 // set the Location header to the UTI for the newly created course,
 // return 201, no content
-router.post('/courses', asyncHandler(async(req, res) => {
-  res.status(201).json({ message: '/courses POST --- no content ---' });
-}));
+router.post('/courses', [
+  check('title').notEmpty(),
+  check('description').notEmpty()
+],
+  asyncHandler(async (req, res) => {
+    res.status(201).json({ message: '/courses POST --- no content ---' });
+  }));
 
 // PUT route that will update the corresponding course,
 // return 204, no content
-router.put('/courses/:id', asyncHandler(async(req, res) => {
+router.put('/courses/:id', asyncHandler(async (req, res) => {
   res.status(204).json({ message: '/courses/id PUT --- no content ---' });
 }));
 
 // DELETE route that will delete the corresponding course,
 // return 204, no content
-router.delete('/courses/:id', asyncHandler(async(req, res) => {
+router.delete('/courses/:id', asyncHandler(async (req, res) => {
   res.status(204).json({ message: '/courses DELETE --- no content ---' });
 }));
 
