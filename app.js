799bd8f543d25ba5fd9701a35c737a944a6498ef
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-02-01 16:03:19 -0800

    verify connection to db

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const { sequelize } = require('./models');
+
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
 
@@ -46,3 +48,13 @@ app.set('port', process.env.PORT || 5000);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express server is listening on port ${server.address().port}`);
 });
+
+// Test the database connection.
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to database has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
