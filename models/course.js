de5d90c9d9354478b94b757faaef5441fb89a48e
Sean Crooks <115957278+crooks-s@users.noreply.github.com>
2024-02-05 13:00:11 -0800

    add validation to title and description

--- models/course.js
+++ models/course.js
@@ -2,10 +2,32 @@
 const { Model, DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  class Course extends Model {}
+  class Course extends Model { }
   Course.init({
-    title: DataTypes.STRING,
-    description: DataTypes.TEXT,
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please enter a course title.',
+        },
+        notEmpty: {
+          msg: 'A course title is required.'
+        },
+      },
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Please enter a course description.',
+        },
+        notEmpty: {
+          msg: 'A course description is required.',
+        },
+      },
+    },
     estimatedTime: DataTypes.STRING,
     materialsNeeded: DataTypes.STRING,
   }, { sequelize });
